dd813412438b87a18733acbf4987d63d2d960716
MoaazAbdelaziz <moaazwork1@gmail.com>
2023-09-19 14:34:21 +0300

    finish todo tab

---
+++ lib/home/todo/task_item.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:todo_app/home/my_theme.dart';
+
+class TaskItem extends StatelessWidget {
+  const TaskItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Slidable(
+        startActionPane: ActionPane(
+          extentRatio: 0.25,
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              backgroundColor: MyTheme.redColor,
+              foregroundColor: MyTheme.whitColor,
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+          ],
+        ),
+        child: Container(
+          height: 100,
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: MyTheme.whitColor,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              VerticalDivider(
+                thickness: 3,
+                color: MyTheme.primaryLight,
+                indent: 15,
+                endIndent: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Title',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleSmall!
+                            .copyWith(color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Description',
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 21, vertical: 7),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: Icon(
+                  Icons.check,
+                  size: 35,
+                  color: MyTheme.whitColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/home/todo/todo_tab.dart
+++ lib/home/todo/todo_tab.dart
@@ -1,12 +1,37 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:todo_app/home/my_theme.dart';
+import 'package:todo_app/home/todo/task_item.dart';
 
 class TodoTab extends StatelessWidget {
   const TodoTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Todo'),
+    return Column(
+      children: [
+        CalendarTimeline(
+          initialDate: DateTime.now(),
+          firstDate: DateTime.now().subtract(const Duration(days: 365)),
+          lastDate: DateTime.now().add(const Duration(days: 365)),
+          onDateSelected: (date) => print(date),
+          leftMargin: 20,
+          monthColor: MyTheme.blackColor,
+          dayColor: MyTheme.blackColor,
+          activeDayColor: MyTheme.whitColor,
+          activeBackgroundDayColor: Theme.of(context).primaryColor,
+          dotsColor: MyTheme.whitColor,
+          selectableDayPredicate: (date) => true,
+          locale: 'en_ISO',
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) => const TaskItem(),
+            itemCount: 20,
+          ),
+        ),
+      ],
     );
   }
 }

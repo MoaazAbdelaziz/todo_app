f45eb9d8222dc0957f7f7ff6fc5bea18495dedba
MoaazAbdelaziz <moaazwork1@gmail.com>
2023-09-27 15:38:20 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/cache_helper.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/home/home_view.dart';
 import 'package:todo_app/home/my_theme.dart';
 import 'package:todo_app/home/todo/edit_view.dart';
@@ -9,7 +11,7 @@ import 'package:todo_app/providers/app_config_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await CacheHelper.cacheInit();
 
   runApp(
